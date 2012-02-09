cecbf295e6b521824379faefd54f4977fd128a9b
matthias <schmid_mat@gmx.at>
2012-02-09 17:59:40 +0100

    added Table model

---
+++ src/model/Table.cpp
@@ -0,0 +1,25 @@
+/*
+ * Table.cpp
+ *
+ *  Created on: Feb 9, 2012
+ *      Author: matthias
+ */
+
+#include "Table.h"
+
+Table::Table(Ogre::SceneManager* sceneMgr) {
+	mSceneMgr = sceneMgr;
+	setup();
+}
+
+Table::~Table() {
+}
+
+void Table::setup() {
+//		mEntity = mSceneMgr->createEntity("table", "Cube.mesh");
+//		mSceneNode = mSceneMgr->createSceneNode("table");
+//		mSceneMgr->getRootSceneNode()->addChild(mSceneNode);
+//		mSceneNode->attachObject(mEntity);
+//		mSceneNode->scale(1,2,10);
+//		mEntity->setMaterialName("floor");
+}
