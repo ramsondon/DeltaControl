e62ebf7140387a0104e5e4b2886c448d13770846
matthias <schmid_mat@gmx.at>
2012-02-10 13:53:19 +0100

    added 3 monitors

--- src/model/Monitor.cpp
+++ src/model/Monitor.cpp
@@ -23,9 +23,26 @@ void Monitor::setup() {
 	mSceneNode = mSceneMgr->createSceneNode("monitor1");
 	mSceneMgr->getRootSceneNode()->addChild(mSceneNode);
 	mSceneNode->attachObject(mEntity);
-	mSceneNode->setPosition(-ROOM_WIDTH/2+60,55,-30);
+	mSceneNode->setPosition(-ROOM_WIDTH/2+48,55,-20);
 	mSceneNode->yaw(Ogre::Degree(90));
 	mSceneNode->scale(0.00003,0.00003,0.00003);
+
+
+	Ogre::Entity* m2ent = mSceneMgr->createEntity("monitor2", "monitor.mesh");
+	Ogre::SceneNode* m2node = mSceneMgr->createSceneNode("monitor2");
+	mSceneMgr->getRootSceneNode()->addChild(m2node);
+	m2node->attachObject(m2ent);
+	m2node->setPosition(-ROOM_WIDTH/2+60,55,-60);
+	m2node->yaw(Ogre::Degree(50));
+	m2node->scale(0.00003,0.00003,0.00003);
+
+	Ogre::Entity* m3ent = mSceneMgr->createEntity("monitor3", "monitor.mesh");
+	Ogre::SceneNode* m3node = mSceneMgr->createSceneNode("monitor3");
+	mSceneMgr->getRootSceneNode()->addChild(m3node);
+	m3node->attachObject(m3ent);
+	m3node->setPosition(-ROOM_WIDTH/2+100,55,-75);
+	m3node->yaw(Ogre::Degree(0));
+	m3node->scale(0.00003,0.00003,0.00003);
 }
 
 
